8e7c2d83b5f4bb2102f0f65d5793f99d879e6765
Saeyoon Oh <43169204+saeyoon17@users.noreply.github.com>
2024-03-11 23:28:28 +0900

    Update key-gen.sh

--- adt/key-gen.sh
+++ adt/key-gen.sh
@@ -1,10 +1,10 @@
-snarkjs powersoftau new bn128 12 pot12_0000.ptau -v
-snarkjs powersoftau contribute pot12_0000.ptau pot12_0001.ptau --name="First contribution" -v <<EOF
+snarkjs powersoftau new bn128 15 pot15_0000.ptau -v
+snarkjs powersoftau contribute pot15_0000.ptau pot15_0001.ptau --name="First contribution" -v <<EOF
 asdf
 EOF
 
-snarkjs powersoftau prepare phase2 pot12_0001.ptau pot12_final.ptau -v
-snarkjs groth16 setup AuthenticatedDT.r1cs pot12_final.ptau proof0.key
+snarkjs powersoftau prepare phase2 pot15_0001.ptau pot15_final.ptau -v
+snarkjs groth16 setup AuthenticatedDT.r1cs pot15_final.ptau proof0.key
 snarkjs zkey contribute proof0.key proof01.key --name="your name" -v <<EOF
 asdf
 EOF
