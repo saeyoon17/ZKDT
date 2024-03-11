2f9d948cac2b8d00b7d6a8ef2d8624e26e05d467
Saeyoon Oh <saeyoon17@gmail.com>
2024-03-11 23:04:38 +0900

    fix zkdt

--- adt/key-gen.sh
+++ adt/key-gen.sh
@@ -1,10 +1,10 @@
-snarkjs powersoftau new bn128 14 pot14_0000.ptau -v
-snarkjs powersoftau contribute pot14_0000.ptau pot14_0001.ptau --name="First contribution" -v <<EOF
+snarkjs powersoftau new bn128 12 pot12_0000.ptau -v
+snarkjs powersoftau contribute pot12_0000.ptau pot12_0001.ptau --name="First contribution" -v <<EOF
 asdf
 EOF
 
-snarkjs powersoftau prepare phase2 pot14_0001.ptau pot14_final.ptau -v
-snarkjs groth16 setup AuthenticatedDT.r1cs pot14_final.ptau proof0.key
+snarkjs powersoftau prepare phase2 pot12_0001.ptau pot12_final.ptau -v
+snarkjs groth16 setup AuthenticatedDT.r1cs pot12_final.ptau proof0.key
 snarkjs zkey contribute proof0.key proof01.key --name="your name" -v <<EOF
 asdf
 EOF
